ef5e9064d90840c9bcc7e4d90c0ce3b4eeef0392
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-06-02 07:46:17 -0300

    loading snapshot data in folder

--- routes/admin.js
+++ routes/admin.js
@@ -1,23 +1,16 @@
-//? Require the NPM module express
 const express = require('express');
 
-//? Require the controller file so the middleware below knows what to do on each file
-const adminController = require('../controllers/product-controller.js');
+const adminController = require('../controllers/admin');
 
-//? Setup the routing for the required HTML files
 const router = express.Router();
 
-//? /admin/add-product → GET + get product controller in the controllers folder
-router.get('/add-product', adminController.getProduct);
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
-//? /admin/edit-product → GET + get edit product controller in the controllers folder
-router.get('/edit-product', adminController.getEditProduct);
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
-//? /admin/edit-product → GET + get edit product controller in the controllers folder
-router.get('/products-list', adminController.getProductsList);
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
-//? /admin/add-product → POST + post product controller in the controllers folder
-router.post('/add-product', adminController.postProduct);
-
-//? Exporting the router to be accessed in other functions
 module.exports = router;
--- routes/shop.js
+++ routes/shop.js
@@ -1,28 +1,17 @@
-//? NPM module import
 const express = require('express');
 
-//? Pulling the shop-controller file so the products can be served in this page
-const shopController = require('../controllers/shop-controller.js');
+const shopController = require('../controllers/shop');
 
-//? Creation of the router controller from the express framework
 const router = express.Router();
 
-//? Welcome the user to the main page, with no items displayed
-router.get('/', shopController.getHome);
+router.get('/', shopController.getIndex);
 
-//? Display all items on sale to the user
-router.get('/products', shopController.getShop);
+router.get('/products', shopController.getProducts);
 
-//? Show the user their shopping cart with whatever items they might have added
 router.get('/cart', shopController.getCart);
 
-//? Display additional details on any inspected product
-router.get('/product-detail', shopController.getItemDetails);
-
-//? Show post-purchase data and delivery details
-router.get('/checkout', shopController.getCheckout);
-
-//? Show post-purchase data and delivery details
 router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
