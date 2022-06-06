d38e1ebeb8b59a3f4eaddf2870f0807f35487c2a
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-06-06 04:51:02 -0300

    added routing and controller to edit product

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,6 @@
 const Product = require('../models/product');
 
+//? Loads the blank page to add a new product rather than editing an old one
 exports.getAddProduct = (req, res) => {
 	res.render('admin/edit-product', {
 		pageTitle: 'Add Product',
@@ -7,6 +8,7 @@ exports.getAddProduct = (req, res) => {
 	});
 };
 
+//? Processes the data request of adding a product
 exports.postAddProduct = (req, res) => {
 	const title = req.body.title;
 	const imageUrl = req.body.imageUrl;
@@ -17,13 +19,34 @@ exports.postAddProduct = (req, res) => {
 	res.redirect('/');
 };
 
+//? Fills the forms with data of a given product and allows editing it
+//! Query is the part after the "?" that you can pass arguments to
 exports.getEditProduct = (req, res) => {
-	res.render('admin/edit-product', {
-		pageTitle: 'Add Product',
-		path: '/admin/add-product',
+	//? Logs the whole query for fun
+	console.log(req.query);
+
+	//? Check if the edit value in query is set to true and redirect if not
+	const editMode = req.query.edit;
+	if (!editMode) {
+		return res.redirect('/');
+	}
+
+	//?
+	const prodId = req.params.productId;
+	Product.findBySingleId(prodId, (product) => {
+		if (!product) {
+			return res.redirect('/');
+		}
+		res.render('admin/edit-product', {
+			pageTitle: 'Edit Product',
+			path: '/admin/edit-product',
+			editing: editMode,
+			item: product,
+		});
 	});
 };
 
+//? Loads the page to view all products in admin mode (with edit)
 exports.getProducts = (req, res) => {
 	Product.fetchAll((products) => {
 		res.render('admin/products', {
--- routes/admin.js
+++ routes/admin.js
@@ -4,13 +4,18 @@ const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-// /admin/add-product => GET
-router.get('/add-product', adminController.getAddProduct);
-
-// /admin/products => GET
+//? Loads all products but on the admin end
 router.get('/products', adminController.getProducts);
 
-// /admin/add-product => POST
+//? Loads the page with the form to add a new item. Despite the route being
+//? to "/add", this will actually load "/edit" as "/add" was deleted previously
+router.get('/add-product', adminController.getAddProduct);
+
+//? Handles the request to adding a new item to the database
 router.post('/add-product', adminController.postAddProduct);
+
+//? Edit a specific product that already belongs to the database
+//? while supplying all of its initial data to hasten the edit process
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
 module.exports = router;
