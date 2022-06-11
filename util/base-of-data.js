6e9b1cdeb170ff613c4ad7328665b8875a379733
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-06-11 16:15:11 -0300

    Removed persistent logging at every single moment

--- util/base-of-data.js
+++ util/base-of-data.js
@@ -1,5 +1,5 @@
 const Sequelize = require('sequelize');
 
-const seq = new Sequelize('first-sql-dn', 'root', 'adminroot', { dialect: 'mysql', host: 'localhost' });
+const seq = new Sequelize('first-sql-dn', 'root', 'adminroot', { dialect: 'mysql', host: 'localhost', logging: false });
 
 module.exports = seq;
