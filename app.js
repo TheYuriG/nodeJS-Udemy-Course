a27dd17acff201b5418cf65fb8c28bb5068248a2
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-06-12 19:27:44 -0300

    Connected to MongoDB and became operational again

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 
 //? Project imports
 const errorController = require('./controllers/error');
+const mongoDB = require('./util/base-of-data');
 
 //? Starts express
 const app = express();
@@ -16,8 +17,8 @@ app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 //? Separates the routes for shop and admin-related pages
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 //? Automatically parses body messages, so other commands can use req.body
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,10 +27,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 //? Only start the routes after the bodyparser has been made available and
 //? the CSS files are made public
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 app.use(errorController.get404);
 
-//? Sets up which port this website will be displayed to on localhost
-//? if the database fully connects as it should
-app.listen(3000);
+mongoDB((client) => {
+	// console.log(client);
+	//? Sets up which port this website will be displayed to on localhost
+	//? if the database fully connects as it should
+	app.listen(3000, () => {
+		console.log('Server listening on port 3000');
+	});
+});
--- util/base-of-data.js
+++ util/base-of-data.js
@@ -1,17 +1,29 @@
-'tozY1rQ8LktyZETy';
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-'mongodb+srv://NodeJS-course:tozY1rQ8LktyZETy@nodejs-tutorial.nsxgg.mongodb.net/?retryWrites=true&w=majority';
+const mongoConnect = (mongoConnectionCallback) => {
+	MongoClient.connect(
+		'mongodb+srv://NodeJS-course:tozY1rQ8LktyZETy@nodejs-tutorial.nsxgg.mongodb.net/?retryWrites=true&w=majority'
+	)
+		.then((client) => {
+			console.log('Connected to MongoDB servers!');
+			mongoConnectionCallback(client);
+		})
+		.catch((error) => console.log(error));
+};
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri =
-	'mongodb+srv://NodeJS-course:tozY1rQ8LktyZETy@nodejs-tutorial.nsxgg.mongodb.net/?retryWrites=true&w=majority';
-const client = new MongoClient(uri, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
-	serverApi: ServerApiVersion.v1,
-});
-client.connect((err) => {
-	const collection = client.db('test').collection('devices');
-	// perform actions on the collection object
-	client.close();
-});
+module.exports = mongoConnect;
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri =
+// 	'mongodb+srv://NodeJS-course:tozY1rQ8LktyZETy@nodejs-tutorial.nsxgg.mongodb.net/?retryWrites=true&w=majority';
+// const client = new MongoClient(uri, {
+// 	useNewUrlParser: true,
+// 	useUnifiedTopology: true,
+// 	serverApi: ServerApiVersion.v1,
+// });
+// client.connect((err) => {
+// 	const collection = client.db('test').collection('devices');
+// 	// perform actions on the collection object
+// 	client.close();
+// });
