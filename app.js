58f46adc18e795172c17b8352661c52792ea4fcf
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-06-15 05:03:35 -0300

    created User class and added its data to requests

--- app.js
+++ app.js
@@ -19,11 +19,26 @@ app.set('views', 'views');
 //? Separates the routes for shop and admin-related pages
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const User = require('./models/user');
 
 //? Automatically parses body messages, so other commands can use req.body
 app.use(bodyParser.urlencoded({ extended: false }));
 //? Enables the css folders to be publicly accessed at any point
 app.use(express.static(path.join(__dirname, 'public')));
+
+//? This is a middleware to attach user data to the request object, so all
+//? requests possess this data and then you can proceed creating carts and
+//? orders with this user
+app.use((req, res, next) => {
+	//? Here we manually created the user on Mongo Compass and then fetched its
+	//? data and hardcoded into this class method
+	User.findById('62a9906875a379ee5e7ffe2e')
+		.then((user) => {
+			req.user = user;
+		})
+		.then((e) => console.log(e));
+	next();
+});
 
 //? Only start the routes after the bodyparser has been made available and
 //? the CSS files are made public
@@ -35,6 +50,7 @@ mongoDB.mongoConnect(() => {
 	//? Sets up which port this website will be displayed to on localhost
 	//? if the database fully connects as it should
 	app.listen(3000, () => {
+		// User.findById('62a9906875a379ee5e7ffe2e');
 		console.log('Server listening on port 3000');
 	});
 });
--- models/user.js
+++ models/user.js
@@ -1,1 +1,24 @@
+const mongoDB = require('mongodb');
+const { getDB } = require('../util/base-of-data');
+
+class User {
+	constructor(email, username, password) {
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+
+	save() {
+		const db = getDB();
+		return db
+			.collection('users') //? users is our accounts table
+			.insertOne(this); //? this is the just created object
+	}
+
+	static findById(userID) {
+		const db = getDB();
+		return db.collection('users').findOne({ _id: new mongoDB.ObjectId(userID) });
+	}
+}
+
 module.exports = User;
