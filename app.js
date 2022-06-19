0d5181e13f3ed2dec72ca01fa28c547fc5e5def9
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-06-18 21:23:05 -0300

    added User to all incoming requests

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('views', 'views');
 //? Separates the routes for shop and admin-related pages
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 //? Automatically parses body messages, so other commands can use req.body
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -29,14 +29,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 //? This is a middleware to attach user data to the request object, so all
 //? requests possess this data and then you can proceed creating carts and
 //? orders with this user
-// app.use((req, res, next) => {
-// 	//? Here we manually created the user on Mongo Compass and then fetched its
-// 	//? data and hardcoded into this class method
-// 	User.findById('62a9906875a379ee5e7ffe2e').then((user) => {
-// 		req.user = new User(user.email, user.username, user.password, user.cart, user._id);
-// 		next();
-// 	});
-// });
+app.use((req, res, next) => {
+	//? Here we manually created the user on Mongo Compass and then fetched its
+	//? data and hardcoded into this class method
+	User.findById('62ae6c054ccbec553949b3d7').then((user) => {
+		req.user = user;
+		next();
+	});
+});
 
 //? Only start the routes after the bodyparser has been made available and
 //? the CSS files are made public
@@ -52,6 +52,15 @@ mongoose
 		//? Sets up which port this website will be displayed to on localhost
 		//? if the database fully connects as it should
 		app.listen(3000, () => {
+			//? We create a new user on first login and then fetch from the
+			//? database so their ID can be used on every request
+			// const user = new User({
+			// 	email: 'user@domain.com',
+			// 	name: 'user',
+			// 	password: 'password',
+			// 	cart: { items: [] },
+			// });
+			// user.save();
 			console.log('Server listening on port 3000');
 		});
 	})
