cf02827668ae5d0c1468ee002c7037c7f36db9ea
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-06-18 21:17:04 -0300

    updated and commented User model

--- models/user.js
+++ models/user.js
@@ -1,187 +1,24 @@
-const mongoDB = require('mongodb');
+//? Require mongoose to setup the document schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-class User {
-	constructor(email, username, password, cart, id) {
-		this.username = username;
-		this.password = password;
-		this.email = email;
-		this.cart = cart ? cart : { items: [] };
-		this._id = id;
-	}
+//? Create a new schema for how an User is defined
+const schemaForUsers = new Schema({
+	name: { type: String, required: true }, //? All users need a name
+	email: { type: String, required: true }, //? All users need an email
+	cart: {
+		//? All users need a cart
+		items: [
+			{
+				productId: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+				//? Carts are composed of a product ID that will point to a product, but
+				//? won't hold the product data itself
+				quantity: { type: Number, required: true }, //? The cart should also hold
+				//? the quantity of the product
+			},
+		],
+	},
+});
 
-	save() {
-		const db = getDB();
-		return db
-			.collection('users') //? users is our accounts table
-			.insertOne(this); //? this is the just created object
-	}
-
-	static findById(userID) {
-		//? Connects to the database
-		const db = getDB();
-
-		//? Finds the user with the given ID
-		return db.collection('users').findOne({ _id: new mongoDB.ObjectId(userID) });
-	}
-
-	addToCart(product) {
-		const db = getDB();
-		//? Cycle through the cart items array and check if the product is
-		//? already in the cart and increase quantity if so.
-		//? If not, add with quantity = 1
-		const cartIndex = this.cart.items.findIndex((item) => item.productId.toString() === product._id.toString());
-
-		//? Check if the item is already in the cart (index will be -1 if not)
-		if (cartIndex >= 0) {
-			//? Increase the quantity of the item in the cart
-			this.cart.items[cartIndex].quantity++;
-
-			//? Update the cart in the database and return as promise
-			return db
-				.collection('users')
-				.updateOne({ _id: new mongoDB.ObjectId(this._id) }, { $set: { cart: this.cart } })
-				.then()
-				.catch((err) => console.log(err));
-		}
-
-		//? If the item doesn't exist in the cart, push to it with quantity = 1
-		this.cart.items.push({ productId: new mongoDB.ObjectId(product._id), quantity: 1 });
-		return db
-			.collection('users')
-			.updateOne({ _id: new mongoDB.ObjectId(this._id) }, { $set: { cart: this.cart } })
-			.then()
-			.catch((err) => console.log(err));
-	}
-
-	getCart() {
-		//? Connects to the database
-		const db = getDB();
-
-		//? Creates an array with all the products ID, so those can be used
-		//? later to retrieve each individual product
-		const productsArray = this.cart.items.map((i) => i.productId);
-
-		return (
-			db
-				.collection('products')
-				//? Search through the database with the IDs in the productsArray
-				.find({ _id: { $in: productsArray } })
-				.toArray() //? The database returns a pointer to each product
-				//? which we then convert to an array
-				.then((arrayOfProducts) => {
-					//? Checks if this operation returned as many items as there
-					//? were in the cart. If not, that means that some of the products
-					//? were deleted from the database, so we need to remove them from
-					//? the cart.
-					if (this.cart.items.length != arrayOfProducts.length) {
-						//? If the lenght of the cart is not equal to the length of the
-						//? array of products, then some of the products were deleted
-						//? and we need to fix that now.
-						console.log(
-							'Cart data mismatch! Some items were deleted from database, but not from the cart. Now removing deleted products from cart...'
-						);
-
-						//? Creates a new array that will store only the cart items that
-						//? are still in the database
-						let newFilteredArray = [];
-
-						//? Cycle through the cart items and check if the product is
-						//? still in the database. If so, add it to the new array.
-						this.cart.items.forEach((item) => {
-							if (
-								arrayOfProducts.find((product) => product._id.toString() === item.productId.toString())
-							) {
-								newFilteredArray.push(item);
-							}
-						});
-
-						//? Update the cart in the database containing only the products
-						//? that are still in the database. After finishing, log the success
-						//? or failure of the operation.
-						db.collection('users')
-							.updateOne(
-								{ _id: new mongoDB.ObjectId(this._id) },
-								{ $set: { cart: { items: newFilteredArray } } }
-							)
-							.then(() => {
-								console.log('Cart data mismatch fixed.');
-							})
-							.catch(() => console.log('Error fixing cart data mismatch.'));
-					}
-
-					//? Then we attach the quantity to each product
-					return arrayOfProducts.map((product) => {
-						//? Cycle through this copied cart for each product,
-						//? match each product from the database to the product
-						//? from the cart and attach the quantity to the cart product
-						for (let i = 0; i < this.cart.items.length; i++) {
-							if (this.cart.items[i].productId.toString() === product._id.toString()) {
-								//? If the product ID matches the ID in the cart
-								//? attach the quantity to the product
-								return { ...product, quantity: this.cart.items[i].quantity };
-							}
-						}
-					});
-				})
-		);
-	}
-
-	//? Processes the removal of one item from the cart when the user
-	//? clicks the "Yeet this item" button
-	removeFromCart(productId) {
-		const db = getDB();
-		//? Find the product in the cart
-		const productIndex = this.cart.items.findIndex((item) => item.productId.toString() === productId.toString());
-		this.cart.items.splice(productIndex, 1);
-		return db
-			.collection('users')
-			.updateOne({ _id: new mongoDB.ObjectId(this._id) }, { $set: { cart: this.cart } })
-			.then()
-			.catch((err) => console.log(err));
-	}
-
-	//? This is the function that processes the "Order now!" button at the cart
-	turnCartIntoOrder(fullyPopulatedCart) {
-		//? Connect to the database first
-		const db = getDB();
-
-		//? Save to the database the order that is being completed from
-		//? the current cart. This is important because the price at the moment
-		//? of purchase is what matters for us, since it won't matter if the
-		//? item has its price changed in the future. The user paid what they paid
-		return db
-			.collection('orders')
-			.insertOne({ userID: this._id, cart: fullyPopulatedCart })
-			.then(() => {
-				//? After you save this order in the database,
-				//? empty this user's cart and save that information to
-				//? the database
-				this.cart = { items: [] };
-				return db
-					.collection('users')
-					.updateOne({ _id: new mongoDB.ObjectId(this._id) }, { $set: { cart: this.cart } });
-			})
-			.catch((err) => console.log(err));
-	}
-
-	//? Process and return the orders data when the user tries to access the
-	//? orders page
-	pullOrders() {
-		//? Connect to the database first
-		const db = getDB();
-
-		//? Find all the orders that belong to the user
-		return (
-			db
-				.collection('orders')
-				.find({ userID: new mongoDB.ObjectId(this._id) })
-				.toArray() //? The database returns a pointer to each order,
-				//? so we need to turn the pointers into an array
-				.then((orders) => orders) //? Return the matrix of orders
-				//? with array of purchased items
-				.catch((e) => console.log(e))
-		);
-	}
-}
-
-module.exports = User;
+//? Export the user model to be used in the app
+module.exports = mongoose.model('User', schemaForUsers);
