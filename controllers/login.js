52a7167e1687c5846800c793a77c24477d17e901
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-06-20 07:12:58 -0300

    further improved and commented (pointless) cookies

--- controllers/login.js
+++ controllers/login.js
@@ -8,6 +8,10 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-	res.setHeader('Set-Cookie', 'completedAuthentication=true');
+	//? First piece of data is the parameters and values of this cookie.
+	//? Second piece is how long this cookie should be usable before it
+	//? expires. Third piece is restricting this cookie to Http requests,
+	//? as that protects from injection attacks (more secure)
+	res.setHeader('Set-Cookie', 'completedAuthentication=true; Max-Age=604800; HttpOnly');
 	res.redirect('/');
 };
