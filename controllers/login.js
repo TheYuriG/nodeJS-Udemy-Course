a58b8ef45d1d304841a464538709c0d6ce7c9cd0
TheYuriG <63727493+TheYuriG@users.noreply.github.com>
2022-06-20 19:20:33 -0300

    fully commented both login route and controller

--- controllers/login.js
+++ controllers/login.js
@@ -1,5 +1,7 @@
+//? Imports the User model so the User model is attached to every request.session
 const User = require('../models/user');
 
+//? Loads the login page for the client
 exports.getLogin = (req, res, next) => {
 	const loginData = req.session.isAuthenticated ? true : false;
 	res.render('auth/authenticate', {
@@ -9,16 +11,23 @@ exports.getLogin = (req, res, next) => {
 	});
 };
 
+//? Processes the login request from the client at "/authenticate"
 exports.postLogin = (req, res, next) => {
-	req.session.isAuthenticated = true;
+	//? Pulls the data from the User model for this user and then
+	//? attach this data to req.session for usage in further requests
 	User.findById('62ae6c054ccbec553949b3d7').then((user) => {
 		req.session.user = user;
+		req.session.isAuthenticated = true;
+		//? Redirects to main once done
 		res.redirect('/');
 	});
 };
 
+//? Processes clicking the "Logout" button on the navigation bar
 exports.postLogout = (req, res, next) => {
-	req.session.destroy().then(() => {
+	//? Destroys this user's session and forces them to login again
+	req.session.destroy(() => {
+		//? While you can Logout from anywhere, doing so will redirect you to home
 		res.redirect('/');
 	});
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,20 @@
+//? NPM import
 const express = require('express');
 
+//? Imports the controllers for authentication that will deal with
+//? requests, responses and rendering
 const loginController = require('../controllers/login');
 
+//? Creates the router responsible for handling authentication
 const router = express.Router();
 
+//? This is the login page
 router.get('/authenticate', loginController.getLogin);
 
+//? This is the login attempt when clicking "Login" in "/authenticate"
 router.post('/login-attempt', loginController.postLogin);
 
+//? This is the logoff request, by clicking "Logout" after being logged in
 router.post('/disconnect', loginController.postLogout);
 
 module.exports = router;
